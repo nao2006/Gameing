f204df5074d42329ca82854ddb8f7448cdf975c0
nao2006 <40855877+nao2006@users.noreply.github.com>
2018-10-04 15:19:36 +0900

    game10

--- ゲーム1/ゲーム1/Enemy.cpp
+++ ゲーム1/ゲーム1/Enemy.cpp
@@ -1,13 +1,14 @@
 #include "Enemy.h"
 #include "Typedef.h"
 #include "DxLib.h"
-#include "Typedef.h"
+
 
 int Dragon;
 Enemy::Enemy()
 {
 	size = {48,48};
 	pos = {200, 100};
+	lpos = pos;
 	hp = 5;
 	Dragon = LoadGraph("image/ƒhƒ‰ƒSƒ“.png");
 	flam = 0;
@@ -21,10 +22,23 @@ Enemy::~Enemy()
 
 void Enemy::Draw()
 {
-	DrawRectGraph(]pos.x, pos.y, size.x, size.y, size.x, size.y, Dragon, true, false);;
+	//DrawRectGraph(pos.x, pos.y, size.x, size.y, size.x, size.y, Dragon, true, false);
+
+	DrawRectGraph(lpos.x, lpos.y,
+		size.x * (animindex % 3), size.y,
+		size.x, size.y, Dragon, TRUE, FALSE);
+
+	flam++;
+
+	if (flam >= 7)
+	{
+		animindex = (animindex + 1) < 11 ? ++animindex : 0;
+		flam = 0;
+	}
 }
 
 void Enemy::UpData()
 {
+
 
 }
